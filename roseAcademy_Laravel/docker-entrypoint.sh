ae7c069b89e9f9c9d489a1b74d971720dec6b304
Dark_Desktop <abdu.ewian@gmail.com>
2026-08-03 05:00:08 +0300

    fixing cors proplem

--- roseAcademy_Laravel/docker-entrypoint.sh
+++ roseAcademy_Laravel/docker-entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 cd /var/www/html
 mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views storage/logs bootstrap/cache
+chown -R www-data:www-data storage bootstrap/cache
 chmod -R ug+rwX storage bootstrap/cache
 
 if [ ! -f .env ]; then
